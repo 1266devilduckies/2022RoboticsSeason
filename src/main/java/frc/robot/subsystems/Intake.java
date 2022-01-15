b3f2c2401352d7335d75c5144958197a8810b0d9
55sly <46945122+1266devilduckies@users.noreply.github.com>
2022-01-15 13:12:15 -0800

    update

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -8,10 +8,7 @@ public class Intake extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
   
-  public static void intakeMotor{
-  
-  
-  }//intakeMotor
+
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
