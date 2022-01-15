e4425f06495cc0cec44fba83f54db859511c347c
55sly <90879690+55sly@users.noreply.github.com>
2022-01-15 11:12:03 -0800

    Update RobotMap.java

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -10,12 +10,13 @@ public class RobotMap{
   public static TalonFX MainLeftMotorFront;
   public static TalonFX MainRightMotorBack;
   public static TalonFX MainRightMotorFront;
+  public static TalonFX IntakeMotor1;
 
   public static void init(){
     MainLeftMotorBack = new TalonFX(0);
     MainLeftMotorFront = new TalonFX(1);
     MainRightMotorBack = new TalonFX(2);
     MainRightMotorFront = new TalonFX(3);
-    
+    IntakeMotor1 = new TalonFX(4);
   }
 }
