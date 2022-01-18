780e922a4bff45e148489430e90f99e989fb2aca
55sly <90879690+55sly@users.noreply.github.com>
2022-01-18 15:28:46 -0800

    Create PewPewStop.java

---
+++ src/main/java/frc/robot/commands/PewPewStop.java
@@ -0,0 +1,44 @@
+package frc.robot.commands;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+
+
+public class PewPewStop extends Command {//--------------class--------------
+  public PewPewStop() {
+    // Use requires() here to declare subsystem dependencies
+    requires(Robot.Shooter);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    RobotMap.PewPewMotor1.set(ControlMode.PercentOutput,0);
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+  
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+  
+  }//class PewPewStop
