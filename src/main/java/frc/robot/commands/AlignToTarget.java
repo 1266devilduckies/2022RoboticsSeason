e98ceb6826df1a7b548f010a6d3731492eeb293c
1266devilduckies <46945122+1266devilduckies@users.noreply.github.com>
2022-03-08 07:53:42 -0800

    integrated align PID to arcadeDrive differential paradigm

--- src/main/java/frc/robot/commands/AlignToTarget.java
+++ src/main/java/frc/robot/commands/AlignToTarget.java
@@ -17,7 +17,6 @@ public class AlignToTarget extends Command {
   @Override
   protected void initialize() {
     RobotMap.isAligningCoroutine = true;
- 
   }
 
   // Called repeatedly when this Command is scheduled to run
@@ -29,7 +28,8 @@ public class AlignToTarget extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    RobotMap.isAligningCoroutine = false;
+    return true;
   }
 
   // Called once after isFinished returns true
